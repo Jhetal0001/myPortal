4f69355904d9156cd151bc319aa416b16f42458a
Jhetal0001 <jhetal00@gmail.com>
2022-12-30 11:31:43 -0500

    Se realiza header y Footer y home

--- proyecto2/src/app/app.module.ts
+++ proyecto2/src/app/app.module.ts
@@ -6,11 +6,17 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './home/header/header.component';
+import { FooterComponent } from './home/footer/footer.component';
+import { MenuComponent } from './home/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
